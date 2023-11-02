30202648b6ed46d62c5d32584e21019556f65e9f
Manuel Zavatta <manuel.zavatta@cogne.com>
2023-11-02 17:06:01 +0100

    add selector

---
+++ frontend/src/app/views/game/selector/selector.component.ts
@@ -0,0 +1,34 @@
+import {Component, Input, OnInit} from '@angular/core';
+import * as dayjs from "dayjs";
+
+@Component({
+  selector: 'app-selector',
+  templateUrl: './selector.component.html',
+  styleUrls: ['./selector.component.scss']
+})
+export class SelectorComponent implements OnInit{
+
+	@Input() uid:string = 'latest';
+
+	current:string|undefined;
+	previous:string|undefined;
+	next:string|undefined;
+
+	ngOnInit():void {
+		let uidDate = new Date();
+		if(this.uid != 'latest'){ uidDate = new Date(this.uid); }
+		this.current = dayjs(uidDate).format('YYYY-MM-DD');
+		let todayDate:Date = new Date();
+		let today:string = dayjs(todayDate).format('YYYY-MM-DD');
+		this.previous = dayjs(uidDate).subtract(1,'day').format('YYYY-MM-DD');
+		if(this.current!=today){
+			this.next = dayjs(uidDate).add(1,'day').format('YYYY-MM-DD');
+		}
+		console.log(this.previous,this.current,this.next);
+	}
+
+	changeGame(uid:string|undefined):void {
+		if(uid){ window.location.href = '/Game/' + uid; }
+	}
+
+}
