36a27763840d7d8f121d2d3f2fe1a8dcb01e45d5
Manuel Zavatta <manuel.zavatta@cogne.com>
2023-11-02 15:37:56 +0100

    update input component

--- frontend/src/app/views/game/input/input.component.ts
+++ frontend/src/app/views/game/input/input.component.ts
@@ -1,12 +1,29 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss']
 })
-export class InputComponent {
+export class InputComponent implements OnChanges {
 
   @Input() word:string = '';
+  @Input() class:string = '';
+
+	size:string = '64px';
+
+	ngOnChanges( changes: SimpleChanges ):void {
+		//console.log(changes);
+		if('word' in changes){
+			const start:number = 64;
+			let size:number = start;
+			let length = changes['word'].currentValue.length;
+			if( length ) {
+				size = start - ( length * 3 );
+				if( size < 27 ){ size = 27; }
+			}
+			this.size = size + 'px';
+		}
+	}
 
 }
