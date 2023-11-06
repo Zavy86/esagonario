d03be001a219379887db4e6e3e016588b58086e5
Manuel Zavatta <manuel.zavatta@cogne.com>
2023-11-06 15:23:05 +0100

    update nickname

--- frontend/src/app/views/game/nickname/nickname.component.ts
+++ frontend/src/app/views/game/nickname/nickname.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Output} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {SessionService} from "src/app/services/session.service";
 
 @Component({
@@ -9,6 +9,8 @@ import {SessionService} from "src/app/services/session.service";
 export class NicknameComponent {
 
 	@Output() nicknameChanged:EventEmitter<string> = new EventEmitter();
+
+	@Input() rank:number = 0;
 
 	nickname:string;
 
